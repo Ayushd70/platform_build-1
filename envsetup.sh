StatiXOS/android_build

build: properly call roomservice
Change-Id: I64dc9eea55e1e244abbec4a57c3a33ac01edbd52

envsetup.sh
@@ -661,7 +661,7 @@ function lunch()
         if [[ $NO_ROOMSERVICE == true ]]; then
             echo "Roomservice turned off, type in 'export NO_ROOMSERVICE=false' if you want it back on"
         else
-            vendor/statix/build/tools/roomservice.py $product
+            python3 vendor/statix/build/tools/roomservice.py $product
         fi
         popd > /dev/null
         check_product $product

Ayushd70/platform_build-1

envsetup.sh
@@ -651,13 +651,13 @@ function lunch()
         # if we can't find a product, try to grab it
         T=$(gettop)
         cd $T > /dev/null
-        vendor/lucid/build/tools/roomservice.py $product
+        python3 vendor/lucid/build/tools/roomservice.py $product
         cd - > /dev/null
         check_product $product
     else
         T=$(gettop)
         cd $T > /dev/null
-        vendor/lucid/build/tools/roomservice.py $product true
+        python3 vendor/lucid/build/tools/roomservice.py $product true
         cd - > /dev/null
     fi
 